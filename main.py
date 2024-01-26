7820dc3bbcdb83a0c97ce7ca6dc86a74836c4aa7
NanayawAgyeman <agyemannana00@gmail.com>
2024-01-26 17:41:37 +0000

    imported language models and agents

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import streamlit as st
+from langchain.agents import create_csv_agent
+from landchain.llms import OpenAI
+from dotenv import load_dotenv
 
 def main():
+
+    load_dotenv()
+    
     st.set_page_config(page_title="Financial Advicer 📈")
     st.header("Financial Advicer 📈")
 
-    user_csv = st.file_uploader("upload your csv file", type=csv)
+    user_csv = st.file_uploader("upload your csv file", type="csv")
+
+    if user_csv is not None:
+        user_question = st.text_input("Ask a question about the csv: ")
 
 
-if __name__  == '__main__'":
+if __name__  == '__main__':
     main()
